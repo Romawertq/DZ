0a5d81013d23bbe53923d22c0eb66bd0128c019d
Romawertq <plitynplitin10@gmail.com>
2025-01-03 22:02:48 +0300

    Начало

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,25 @@
-﻿// На масиивы
+﻿// На массивы
 
+// Поворот
+int[] t = new int[5] {1, 2, 3, 4, 5};
+int[] e = new int[5];
+Console.WriteLine();
+int s = Convert.ToInt32(Console.ReadLine());
+s = s % t.Length;
+Console.WriteLine(" ");
+for (int i = 0; i < 5; i++)
+{
+    e[(i + s) % t.Length] = t[i];
+}
+foreach (int i in e)
+{
+    Console.WriteLine(i);
+}
+// Удаление дубликатов
+int[] q = new int[6] {1, 2, 3, 4, 4, 5};
+int[] w = new int[];
+
+for (int i = 0; i < 6; i++)
+{
+
+}
